0e152963c24a433bcbd9710440ddbff7f1596e1d
Chris Bentzel <cbentzel@gmail.com>
2022-02-09 14:19:33 -0500

    Add more descriptions

--- constants/data.js
+++ constants/data.js
@@ -10,37 +10,43 @@ const userData = {
       title: "Spot",
       link: "https://bostondynamics.com/products/spot",
       imgUrl: "/spot.jpg",
-      description: "I'm the Director of Software for Spot. This is "
+      description: "I'm the Director of Software for Spot, Boston Dynamics' first commercial robot. My team works on everything from firmware up to UIs. There's a ton of interesting technical challenges such as real-time performance for robot software, efficient use of networking resources, and measuring and improving the reliability of the manufacturing process. We're also building out a new product category in conjunction with many partners. Contact me if you're interested in discussing open positions."
     },
     {
       title: "Chrome",
       link: "https://google.com/chrome",
       imgUrl: "/google_chrome.jpeg",
+      description: "I was a Director of Engineering for Chrome, focused on networking and advertising on the web. This was my first foray into management and organizational leadership, and I learned a lot along the way. My team had bottom-line impact on Google's business, as well as advancing the state of the Internet through the creation of protocols like HTTP/2 and HTTP/3, the Better Ads Standard, and kicking off work on improving privacy for trackers."
     },
     {
       title: "YouTube",
       link: "https://www.youtube.com",
       imgUrl: "/youtube.png",
+      description: "I was a Senior Engineer at YouTube. I was part of a small team that built Google's CDN for YouTube from scratch, as well as led early technical work on YouTube's live streaming architecture. One day I took down a significant fraction of YouTube's streaming capacity which led me to learn a lot about postmortems and more resilient architecture."
     },
     {
       title: "Google Talk",
       link: "https://en.wikipedia.org/wiki/Google_Talk",
       imgUrl: "/google_talk.jpeg",
+      description: "I was an Engineer on Google Talk. This was my first exposure to servers, networking, and how to deploy and monitor an Internet Service. I worked on scaling infrastructure as well as AOL Instant Messenger - Google Talk integration."
     },
     {
       title: "Sly Cooper",
       link: "https://en.wikipedia.org/wiki/Sly_Cooper",
       imgUrl: "/sly_cooper.jpeg",
+      description: "I was an engineer on Sly Cooper - from very early prototypes with a tiny team to Tech Lead of Sly Cooper 3. This work was a great trial by fire, taking me from a shoddy engineer to reasonably competent at the end. Since it was a small team, I worked on a wide variety of areas: real-time engine work on PS2, art asset toolchains, game design, and even the occasional bit of animation and script writing. It's been exciting to share this series with my kids many years after it was done and have them still enjoy it."
     },
     {
       title: "Rocket, Robot on Wheels",
       link: "https://en.wikipedia.org/wiki/Rocket:_Robot_on_Wheels",
       imgUrl: "/rocket.jpeg",
+      description: "I joined Sucker Punch towards the tail end of their work on Rocket. I picked up a bunch of random tasks as a result - wrote custom compressors to reduce the size of cartridge needed for the game, made sure all level transitions worked correctly, and many little bug fixes. I'm still happy with the quality of the game, although it led me to dread the words 'cult classic'. "
     },
     {
       title: "3D0",
       link: "https://en.wikipedia.org/wiki/3DO_Interactive_Multiplayer",
       imgUrl: "/3do.jpeg",
+      description: "I joined 3D0 immediately after college, and joined their tiny software R&D team. I built a rigid-body physics simulator to run on 1998-era computers. I then switched to an actual game, which we made from start to finish in about three months. It taught me a lot about how to rapidly develop work and cut corners where possible."
     },
   ],
   projects: [
