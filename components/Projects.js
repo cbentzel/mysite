6acfe7866eca5c18776153d7645e35483813e2e5
Chris Bentzel <cbentzel@gmail.com>
2022-02-09 15:20:15 -0500

    Slightly smaller font

--- components/Projects.js
+++ components/Projects.js
@@ -35,7 +35,7 @@ const ProjectCard = ({ title, link, imgUrl, description }) => {
             </div>
           </div>
         </a>
-        <div className="w-3/5 block text-2xl font-serif font-light text-gray-700 dark:text-gray-300">
+        <div className="w-3/5 block text-xl font-serif font-light text-gray-700 dark:text-gray-300">
           {description}
         </div>
       </div>
