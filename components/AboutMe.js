262e9e0121e4a4ad38731d5b412bac23a376b09f
Chris Bentzel <cbentzel@gmail.com>
2022-02-12 10:22:57 -0500

    A bit more work on main page, more to go

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -2,35 +2,34 @@ import React from "react";
 import userData from "@constants/data";
 
 export default function AboutMe() {
-  const colors = ["#F59E0B", "#84CC16", "#10B981", "#3B82F6"];
   return (
     <section className="bg-white dark:bg-gray-800">
-      <div className="flex flex-row justify-center items-start overflow-hidden bg-[#F1F1F1] dark:bg-gray-900">
+      <div className="flex flex-row items-start overflow-hidden bg-[#F1F1F1] dark:bg-gray-900">
         {/* Text container */}
-        <div className="w-full md:w-1/2 mx-auto text-center content-center md:text-left lg:p-20">
+        <div className="w-1/2 mx-auto text-right lg:p-20">
           <p className="h-32">
           </p>
-          <p className="md:text-right font-sans text-xl">
-            I'm Chris Bentzel - thanks for your interest in learning about me!
+          <p className="font-sans text-xl">
+            Hi - I'm Chris Bentzel!
           </p>
           <p className="h-16">
           </p>
-          <p className="md:text-right font-sans text-xl">
+          <p className="font-sans text-xl">
             I enjoy building great products, as well as the teams that build those products.
           </p>
           <p className="h-16">
           </p>
-          <p className="md:text-right font-sans text-xl">
+          <p className="font-sans text-xl">
             In my personal life, I spend time with my family, exercising outdoors, reading, and working on hobby projects.
           </p>
           <p className="h-16">
           </p>
-          <p className="md:text-right font-sans text-xl">
+          <p className="font-sans text-xl">
             I hope you experience something which makes you smile today.
           </p>
         </div>
         {/* Image container */}
-        <div className="hidden lg:block relative w-full md:w-1/2 -mr-40 mt-20">
+        <div className="lg:block relative w-1/2 -mr-40 mt-20">
           <div className="w-3/4 ">
             <img src={userData.avatarUrl} alt="avatar" className=" shadow" />
             <div className="flex flex-row justify-between mt-4">
