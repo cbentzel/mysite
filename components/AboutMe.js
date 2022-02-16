1a182e1cf6d4569a514d0b31fb1c25f207d5a1b6
Chris Bentzel <cbentzel@gmail.com>
2022-02-15 20:49:32 -0500

    Went to vertical stack

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -5,9 +5,9 @@ import Image from "next/image";
 export default function AboutMe() {
   return (
     <section className="">
-      <div className="flex flex-row self-stretch items-center overflow-hidden gap-8">
+      <div className="flex flex-col self-stretch items-center overflow-hidden gap-8">
         {/* Text container */}
-        <div className="w-1/2 flex flex-col mx-auto justify-evenly text-left pl-4 text-xs sm:text-sm space-y-4 md:text-lg lg:text-xl">
+        <div className="w-full flex flex-col mx-auto justify-evenly text-center pt-4 px-4 text-xs sm:text-sm space-y-4 md:text-lg lg:text-xl">
           <div className="">
             Hi - I'm Chris Bentzel!
           </div>
@@ -22,7 +22,7 @@ export default function AboutMe() {
           </div>
         </div>
         {/* Image container */}
-        <div className="lg:block relative w-1/2">
+        <div className="lg:block relative w-3/4 lg:w-1/2">
             <Image src={userData.avatarUrl} alt="avatar" width="1944" height="1726" placeholder="blur" className="shadow" />
         </div>
       </div>
